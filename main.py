a0e385c8ea10aab247a35ea7e3a94cc9df70bee8
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2023-07-05 10:01:10 -0700

    Create, show, and delete tasks

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 # Ensure templates are auto-reloaded
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
+todos = Base("todos")
+
 
 # Ensure responses aren't cached
 @app.after_request
@@ -15,3 +17,31 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Home Page"""
+
+    return render_template("index.html", todos=todos.fetch().items)
+
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    if request.form.get("name") and request.form.get("desc"):
+        try:
+            todos.put({"desc": request.form.get("desc")}, request.form.get("name"))
+        except:
+            return 500
+        return redirect("/")
+    else:
+        return 400
+
+
+@app.route("/delete", methods=["GET"])
+def delete():
+    try:
+        todos.delete(request.args.get("todo"))
+        return redirect("/")
+    except:
+        return 500
